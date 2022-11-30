6857cfa88eb35194188a3850c24ac39c7006878e
Netanel Basal <netanel7799@gmail.com>
2022-11-30 19:45:27 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { UseQuery } from '@ngneat/query';
+import { map } from 'rxjs';
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface Todo {
+  id: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'webstorm-issue';
+  http = inject(HttpClient);
+  query = inject(UseQuery);
+
+  post$ = this.query(['post'], () => {
+    return this.http.get<Post>('https://jsonplaceholder.typicode.com/posts/1');
+  }).result$;
+
+  todos$ = this.query(['todos'], () => {
+    return this.http
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .pipe(
+        map((todos) => {
+          return {
+            todos,
+          };
+        })
+      );
+  }).result$;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubscribeDirective } from '@ngneat/subscribe';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, SubscribeDirective, NzTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
